ccd79aba7104cbd02a53391251561f03a43b12c0
marlonfurtado <marlonfurtado.rs@gmail.com>
2018-02-11 18:37:06 -0200

    firebase integration

--- src/App.js
+++ src/App.js
@@ -1,15 +1,33 @@
 import React, { Component } from 'react'
-import Header from './components/Header';
+import Header from './components/Header'
 import MessageList from './components/MessageList'
-import logo from './logo.svg'
+import MessageBox from './components/MessageBox'
+import firebase from 'firebase'
+import config from './firebase.env'
 import './App.css'
 
 class App extends Component {
+  constructor(props) {
+    super(props)
+    firebase.initializeApp(config)
+  }
+
   render() {
     return (
       <div className="App">
         <Header title="Hello! Welcome to React." />
-        <MessageList />
+        <div className="columns">
+          <div className="column is-3"></div>
+          <div className="column is-6">
+            <MessageList db={firebase} />
+          </div>
+        </div>
+        <div className="columns">
+          <div className="column is-3"></div>
+          <div className="column is-6">
+            <MessageBox db={firebase} />
+          </div>
+        </div>
       </div>
     )
   }
--- src/components/MessageList.js
+++ src/components/MessageList.js
@@ -1,19 +1,49 @@
-import React, { Component } from 'react';
+import React, { Component } from 'react'
+import Message from './Message'
+import _ from 'lodash'
 
 class MessageList extends Component {
   constructor(props) {
-    super(props);
+    super(props)
     this.state = {
       messages: []
     }
+    let app = this.props.db.database().ref('messages')
+    app.on('value', snapshot => {
+      this.getData(snapshot.val())
+    })
+  }
+
+  getData(values) {
+    let messagesVal = values
+    let messages = _(messagesVal)
+      .keys()
+      .map(messageKey => {
+        let cloned = _.clone(messagesVal[messageKey])
+        cloned.key = messageKey
+        return cloned
+      })
+      .value()
+    this.setState({
+      messages: messages
+    })
   }
 
   render() {
+    let messageNodes = this.state.messages.map((message) => {
+      return (
+        <div className="card">
+          <div className="card-content">
+            <Message message={message.message} />
+          </div>
+        </div>
+      )
+    })
     return (
       <div>
-        MessageList Component
+        {messageNodes}
       </div>
-    );
+    )
   }
 }
 
